62d7930b6ee4848c454c168b573465836d1075ad
Tom <tnorth93@gmail.com>
2018-09-18 13:40:52 -0700

    I think I've made some progress on the transform but the readInt32LE is not being recognized as a function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const transform = require('./lib/transform');
 const parseBitmap = require('./lib/parse-bitmap');
 const readWrite = require('./lib/read-write');
 
-const handleTransform = (existingFile) => {
-  readWrite.read(existingFile, parseBitmap.parse);
-};
+// const handleTransform = (existingFile) => {
+//   readWrite.read(existingFile, parseBitmap.parse);
+// };
+//
+// handleTransform(`${__dirname}/assets/${process.argv[2]}`);
 
-handleTransform(`${__dirname}/assets/${process.argv[2]}`);
+readWrite.read(`${__dirname}/../assets/house.bmp}`, (error, buffer) => {
+  const parsedBitmap = parseBitmap.parse(buffer);
+  // select transformation
+  transform.invert(parsedBitmap);
+  // here your bitmap haps been transformed
+  readWrite.write(parsedBitmap, () => {});
+});
